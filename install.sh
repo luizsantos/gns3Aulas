3fbf1898266c10282e5c72c7077dc8e8caf3db98
luizsantos <luiz.arthur.feitosa.santos@gmail.com>
2021-09-15 16:56:30 -0300

    change install

--- install.sh
+++ install.sh
@@ -2,26 +2,29 @@
 echo "Iniciando script para configuração da VM do GNS3 para as aulas de Redes de Computadores e Cibersegurança da UTFPR-C"
 
 echo "Atualizando lista de pacotes Linux da VM"
-#sudo apt update
+sudo apt update
 
-echo "Gerando links das Appliances do GNS3 para a aula"
-#sudo ln -s appliances/*.gns3a /usr/local/lib/python3.8/dist-packages/gns3server/appliances/
+echo -e "\n\nInstalar os templates da aula? (s)im ou (n)ão? Padrão Não.\n"
+read installTemp
 
-echo "Usar a interface gráfica GNS3-gui para acessar o GNS3?"
-echo "Atenção - Caso você não instale isso, você utilizará apenas a interface Web para usar o GNS3"
-echo "Instalar o interface gráfica GNS3-gui? (S)im ou (N)ão? Padrão Não"
-read installGui
-
-if [ "$installGui" = "Y"] | [ "$installGui" = "S"] | [ "$installGui" = "s"] | [ "$installGui" = "y"]; then
-
-    echo "Preparando o SSH do Linux para acessar X11 via SSH"
-    #sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config-bkp
-    #sudo cp configure/sshd_config /etc/ssh/sshd_config
-
-    echo "Instalando o GNS3-gui, para acessar via SSH"
-    #sudo apt install gns3-gui
+if [ "$installTemp" = "s" ] || [ "$installTemp" = "S" ] || [ "$installTemp" = "y" ] || [ "$installTemp" = "Y" ] ;  then
+    echo "\tGerando links das Appliances do GNS3 para a aula"
+    sudo ln -s appliances/*.gns3a /usr/local/lib/python3.8/dist-packages/gns3server/appliances/
 else
-    echo "Pulando instalão do GNS3-gui..."
+    echo "\tPulando a instalação dos templates..."
 fi
 
+echo -e "\tInstalar a interface gráfica desktop GNS3-gui? (s)im ou (n)ão? Padrão Não.\n"
+echo -e "\tAtenção - Caso você não instale isso, você utilizará apenas a interface Web para usar o GNS3."
+read installGui
 
+if [ "$installGui" = "s" ] || [ "$installGui" = "S" ] || [ "$installGui" = "y" ] || [ "$installGui" = "Y" ] ;  then
+    echo "\tPreparando o SSH do Linux para acessar X11 via SSH"
+    sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config-bkp
+    sudo cp configure/sshd_config /etc/ssh/sshd_config
+
+    echo "\tInstalando o GNS3-gui, para acessar via SSH"
+    sudo apt install gns3-gui
+else
+    echo "\tPulando a instalação do GNS3-gui..."
+fi
