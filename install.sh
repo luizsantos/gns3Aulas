71778543ba7e0b968f34870c4225f13fe194d7c5
luizsantos <luiz.arthur.feitosa.santos@gmail.com>
2021-09-15 17:49:51 -0300

    change install

--- install.sh
+++ install.sh
@@ -8,10 +8,10 @@ echo -e "\n\nInstalar os templates da aula? (s)im ou (n)ão? Padrão Não.\n"
 read installTemp
 
 if [ "$installTemp" = "s" ] || [ "$installTemp" = "S" ] || [ "$installTemp" = "y" ] || [ "$installTemp" = "Y" ] ;  then
-    echo "\tGerando links das Appliances do GNS3 para a aula"
-    sudo ln -s appliances/*.gns3a /usr/local/lib/python3.8/dist-packages/gns3server/appliances/
+    echo -e "\tGerando links das Appliances do GNS3 para a aula"
+    sudo ln -s `pwd`/appliances/*.gns3a /usr/local/lib/python3.8/dist-packages/gns3server/appliances/
 else
-    echo "\tPulando a instalação dos templates..."
+    echo -e "\tPulando a instalação dos templates..."
 fi
 
 echo -e "\tInstalar a interface gráfica desktop GNS3-gui? (s)im ou (n)ão? Padrão Não.\n"
@@ -19,12 +19,12 @@ echo -e "\tAtenção - Caso você não instale isso, você utilizará apenas a i
 read installGui
 
 if [ "$installGui" = "s" ] || [ "$installGui" = "S" ] || [ "$installGui" = "y" ] || [ "$installGui" = "Y" ] ;  then
-    echo "\tPreparando o SSH do Linux para acessar X11 via SSH"
+    echo -e "\tPreparando o SSH do Linux para acessar X11 via SSH"
     sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config-bkp
     sudo cp configure/sshd_config /etc/ssh/sshd_config
 
-    echo "\tInstalando o GNS3-gui, para acessar via SSH"
+    echo -e "\tInstalando o GNS3-gui, para acessar via SSH"
     sudo apt install gns3-gui
 else
-    echo "\tPulando a instalação do GNS3-gui..."
+    echo -e "\tPulando a instalação do GNS3-gui..."
 fi
