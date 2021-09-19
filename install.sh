3c5dbefe80efd2291a4608a33131b678aa262330
luizsantos <luiz.arthur.feitosa.santos@gmail.com>
2021-09-19 03:53:04 -0300

    change install

--- install.sh
+++ install.sh
@@ -57,7 +57,7 @@ echo "\nVerify if $2 exists and it's correct..."
  if [ -f "$2" ]; then
         echo "\nFile $2 already exists..."
         # verify if md5sum files
-        md5FromFile=`md5sum $file7200`
+        md5FromFile=`md5sum $2`
         if [ "$3" = "$md5FromFile" ]; then
             echo "\nMD5 from file $2 is correct..."
         else
