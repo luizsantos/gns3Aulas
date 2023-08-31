5d41180aedc08ac43d946b281fed5c8cf969c257
luizsantos <luiz.arthur.feitosa.santos@gmail.com>
2023-08-31 15:43:23 -0300

    Add files via upload

--- install_centOS_lab.sh
+++ install_centOS_lab.sh
@@ -9,8 +9,8 @@ dirUsu="/home/aluno"
 
 confGNS3local () {
     $eco "\nConfigure GNS3 to run on local"
-    sudo cp -f /lib/systemd/system/gns3.service /lib/systemd/system/gns3.service-def
-    sudo cp -f configure/gns3.service /lib/systemd/system/gns3.service
+#    sudo cp -f /lib/systemd/system/gns3.service /lib/systemd/system/gns3.service-def
+#    sudo cp -f configure/gns3.service /lib/systemd/system/gns3.service
 
     # quando instala a interface gráfica desktop o server muda pedindo a autenticação da interface web, esta configuração vai desabilitar isso e por garantia o usuário/senha vai ser gns3.
     #$eco "\nConfigure GNS3 server web password and auth"
@@ -77,31 +77,33 @@ appliances () {
     #ln -f -s `pwd`/appliances/*.gns3a ~/GNS3/appliances/
 
     # cisco ios
-    sudo mkdir -p /opt/gns3/images/IOS/
+    dirImg="/opt/gns3/images/IOS/"
+    sudo mkdir -p $dirImg 
 
     #download file
 
-    file7200="appliances/c7200-adventerprisek9-mz.124-24.T5.bin"
+    file7200="c7200-adventerprisek9-mz.124-24.T5.bin"
     md5_7200="3c4148f62acf56602ce3b371ebae60c9"
 
-    gDriveDownVerify "1uR5e3nsfgvpRE9bNXSok4rZO4HCkqjET" $file7200 $md5_7200
-    sudo mv `pwd`/appliances/c7200-adventerprisek9-mz.124-24.T5.bin /opt/gns3/images/IOS/
-    soma=`md5sum appliances/c7200-adventerprisek9-mz.124-24.T5.bin | awk '{print $1}'`
+    #gDriveDownVerify "1uR5e3nsfgvpRE9bNXSok4rZO4HCkqjET" $dirImg$file7200 $dirImg$md5_7200
+    #sudo mv `pwd`/appliances/c7200-adventerprisek9-mz.124-24.T5.bin /opt/gns3/images/IOS/
+    soma=`md5sum $dirImg/c7200-adventerprisek9-mz.124-24.T5.bin | awk '{print $1}'`
+    echo $soma do arquivo 7200
     sudo bash -c 'echo '$soma' > /opt/gns3/images/IOS/c7200-adventerprisek9-mz.124-24.T5.bin.md5sum'
 
 
-    file3640="appliances/c3640-a3js-mz.124-25d.image"
+    file3640="c3640-a3js-mz.124-25d.image"
     md5_3640="493c4ef6578801d74d715e7d11596964"
 
     # cisco ios switch (na verdade é um router, mas vamos usar como switch
-    gDriveDownVerify "1sKkWOzx0Cl-TvwGBQufpmmQerAYpSznM" $file3640 $md5_3640
+    gDriveDownVerify "1sKkWOzx0Cl-TvwGBQufpmmQerAYpSznM" $dirImg$file3640 $dirImag$md5_3640
 
-    sudo mv `pwd`/appliances/c3640-a3js-mz.124-25d.image /opt/gns3/images/IOS/
-    soma=`md5sum appliances/c3640-a3js-mz.124-25d.image| awk '{print $1}'`
-    sudo bash -c 'echo '$soma' > /opt/gns3/images/IOS/c3640-a3js-mz.124-25d.image.image.md5sum'
+    #sudo mv `pwd`/appliances/c3640-a3js-mz.124-25d.image /opt/gns3/images/IOS/
+    soma=`md5sum $dirImg/c3640-a3js-mz.124-25d.image| awk '{print $1}'`
+    sudo bash -c 'echo '$soma' > $dirImg/c3640-a3js-mz.124-25d.image.image.md5sum'
 
     # copying imagens to user
-    sudo mkdir -p /home/aluno/GNS3/images/IOS/
+    sudo mkdir -p $dirUsu/GNS3/images/IOS/
     sudo ln -f -s /opt/gns3/images/IOS/* $dirUsu/GNS3/images/IOS/
 
     # add templates and icons
@@ -109,8 +111,8 @@ appliances () {
     sudo cp icons/routerLinux.svg /usr/local/lib/python3.10/dist-packages/gns3server/symbols/classic/
 
     $eco "\nConfigure templates - copynt controller"
-    sudo mkdir $dirUsu/.config/GNS3/2.2/
-    sudo cp configure/gns3_controller.conf $dirUsu/.config/GNS3/2.2/
+    sudo mkdir -p $dirUsu/.config/GNS3/2.2/
+    sudo cp configure/*.conf $dirUsu/.config/GNS3/2.2/
     sudo chown -R aluno.aluno $dirUsu/.config/GNS3/2.2/
 }
 
@@ -133,16 +135,19 @@ fi
 
 #instalar cisto packet tracer
 $eco "\nInstall Cisco Packet Tracer (to access using SSH)? \n[N\y]\n"
-read installPT
-if [ "$installPT" = "s" ] || [ "$installPT" = "S" ] || [ "$installPT" = "y" ] || [ "$installPT" = "Y" ] ;  then
-    $eco "\nInstalling Cisco Packet Tracer.\n"
-    # atualizar linux
-    $eco "\nUpdating Ubuntu mirrors"
-    sudo apt update
-    ciscoPT
-else
-    $eco "\nInstalling Cisco Packet Tracer canceled!!!\n"
-fi
+#read installPT
+#if [ "$installPT" = "s" ] || [ "$installPT" = "S" ] || [ "$installPT" = "y" ] || [ "$installPT" = "Y" ] ;  then
+#    $eco "\nInstalling Cisco Packet Tracer.\n"
+#    # atualizar linux
+#    $eco "\nUpdating Ubuntu mirrors"
+#    sudo apt update
+#    ciscoPT
+#else
+#    $eco "\nInstalling Cisco Packet Tracer canceled!!!\n"
+#fi
+
+$eco "\nmod aluno"
+sudo chown -R aluno:aluno /home/aluno
 
 # finalização
 $eco "\n\nFinished..."
